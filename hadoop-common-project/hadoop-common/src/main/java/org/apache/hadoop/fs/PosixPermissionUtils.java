c577db2bc33e7227c9bdc9bcf837e41d6285d94b
Arun Soman <aarunsoman@gmail.com>
2016-08-18 10:16:11 +0530

    Create PosixPermissionUtils.java
    Support Posix operations

---
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/PosixPermissionUtils.java
@@ -0,0 +1,123 @@
+package org.apache.hadoop.fs;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.hadoop.fs.permission.FsPermission;
+
+public class PosixPermissionUtils {
+
+    public static Set<PosixFilePermission> setPosixPermission(Path p, FsPermission permission) throws IOException {
+        final Set<PosixFilePermission> perms = new HashSet<PosixFilePermission>();
+        synchronized (p) {
+            switch (permission.getUserAction()) {
+            case ALL:
+                perms.add(PosixFilePermission.OWNER_READ);
+                perms.add(PosixFilePermission.OWNER_WRITE);
+                perms.add(PosixFilePermission.OWNER_EXECUTE);
+                break;
+            case EXECUTE:
+                perms.add(PosixFilePermission.OWNER_EXECUTE);
+                break;
+            case NONE:
+
+                break;
+            case READ:
+                perms.add(PosixFilePermission.OWNER_READ);
+                break;
+            case READ_EXECUTE:
+                perms.add(PosixFilePermission.OWNER_READ);
+                perms.add(PosixFilePermission.OWNER_EXECUTE);
+                break;
+            case READ_WRITE:
+                perms.add(PosixFilePermission.OWNER_READ);
+                perms.add(PosixFilePermission.OWNER_WRITE);
+                break;
+            case WRITE:
+                perms.add(PosixFilePermission.OWNER_WRITE);
+                break;
+            case WRITE_EXECUTE:
+                perms.add(PosixFilePermission.OWNER_WRITE);
+                perms.add(PosixFilePermission.OWNER_EXECUTE);
+                break;
+
+            default:
+                break;
+            }
+            switch (permission.getGroupAction()) {
+            case ALL:
+                perms.add(PosixFilePermission.GROUP_READ);
+                perms.add(PosixFilePermission.GROUP_WRITE);
+                perms.add(PosixFilePermission.GROUP_EXECUTE);
+                break;
+            case EXECUTE:
+                perms.add(PosixFilePermission.GROUP_EXECUTE);
+                break;
+            case NONE:
+
+                break;
+            case READ:
+                perms.add(PosixFilePermission.GROUP_READ);
+                break;
+            case READ_EXECUTE:
+                perms.add(PosixFilePermission.GROUP_READ);
+                perms.add(PosixFilePermission.GROUP_EXECUTE);
+                break;
+            case READ_WRITE:
+                perms.add(PosixFilePermission.GROUP_READ);
+                perms.add(PosixFilePermission.GROUP_WRITE);
+                break;
+            case WRITE:
+                perms.add(PosixFilePermission.GROUP_WRITE);
+                break;
+            case WRITE_EXECUTE:
+                perms.add(PosixFilePermission.GROUP_WRITE);
+                perms.add(PosixFilePermission.GROUP_EXECUTE);
+                break;
+
+            default:
+                break;
+            }
+            switch (permission.getOtherAction()) {
+            case ALL:
+                perms.add(PosixFilePermission.OTHERS_READ);
+                perms.add(PosixFilePermission.OTHERS_WRITE);
+                perms.add(PosixFilePermission.OTHERS_EXECUTE);
+                break;
+            case EXECUTE:
+                perms.add(PosixFilePermission.OTHERS_EXECUTE);
+                break;
+            case NONE:
+
+                break;
+            case READ:
+                perms.add(PosixFilePermission.OTHERS_READ);
+                break;
+            case READ_EXECUTE:
+                perms.add(PosixFilePermission.OTHERS_READ);
+                perms.add(PosixFilePermission.OTHERS_EXECUTE);
+                break;
+            case READ_WRITE:
+                perms.add(PosixFilePermission.OTHERS_READ);
+                perms.add(PosixFilePermission.OTHERS_WRITE);
+                break;
+            case WRITE:
+                perms.add(PosixFilePermission.OTHERS_WRITE);
+                break;
+            case WRITE_EXECUTE:
+                perms.add(PosixFilePermission.OTHERS_WRITE);
+                perms.add(PosixFilePermission.OTHERS_EXECUTE);
+                break;
+
+            default:
+                break;
+            }
+        }
+        Files.setPosixFilePermissions(Paths.get(p.toUri()), perms);
+        return perms;
+    }
+}
