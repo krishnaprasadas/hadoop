12109d84987df515b32751b722468c659ab9b9cf
krishnaprasadas <krishnaprasadas87@gmail.com>
2016-08-24 19:16:44 +0530

    Fixed test case issue with special characters in path
    Used RawPath

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/PosixPermissionUtils.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/PosixPermissionUtils.java
@@ -13,132 +13,133 @@ import org.apache.hadoop.fs.permission.FsPermission;
 
 public class PosixPermissionUtils {
 
-    public static Set<PosixFilePermission> setPosixPermission(Path workingDir,Path p, FsPermission permission) throws IOException {
-        final Set<PosixFilePermission> perms = new HashSet<PosixFilePermission>();
-        synchronized (p) {
-            switch (permission.getUserAction()) {
-            case ALL:
-                perms.add(PosixFilePermission.OWNER_READ);
-                perms.add(PosixFilePermission.OWNER_WRITE);
-                perms.add(PosixFilePermission.OWNER_EXECUTE);
-                break;
-            case EXECUTE:
-                perms.add(PosixFilePermission.OWNER_EXECUTE);
-                break;
-            case NONE:
+	public static Set<PosixFilePermission> setPosixPermission(Path workingDir, Path p, FsPermission permission)
+			throws IOException {
+		final Set<PosixFilePermission> perms = new HashSet<PosixFilePermission>();
+		synchronized (p) {
+			switch (permission.getUserAction()) {
+			case ALL:
+				perms.add(PosixFilePermission.OWNER_READ);
+				perms.add(PosixFilePermission.OWNER_WRITE);
+				perms.add(PosixFilePermission.OWNER_EXECUTE);
+				break;
+			case EXECUTE:
+				perms.add(PosixFilePermission.OWNER_EXECUTE);
+				break;
+			case NONE:
 
-                break;
-            case READ:
-                perms.add(PosixFilePermission.OWNER_READ);
-                break;
-            case READ_EXECUTE:
-                perms.add(PosixFilePermission.OWNER_READ);
-                perms.add(PosixFilePermission.OWNER_EXECUTE);
-                break;
-            case READ_WRITE:
-                perms.add(PosixFilePermission.OWNER_READ);
-                perms.add(PosixFilePermission.OWNER_WRITE);
-                break;
-            case WRITE:
-                perms.add(PosixFilePermission.OWNER_WRITE);
-                break;
-            case WRITE_EXECUTE:
-                perms.add(PosixFilePermission.OWNER_WRITE);
-                perms.add(PosixFilePermission.OWNER_EXECUTE);
-                break;
+				break;
+			case READ:
+				perms.add(PosixFilePermission.OWNER_READ);
+				break;
+			case READ_EXECUTE:
+				perms.add(PosixFilePermission.OWNER_READ);
+				perms.add(PosixFilePermission.OWNER_EXECUTE);
+				break;
+			case READ_WRITE:
+				perms.add(PosixFilePermission.OWNER_READ);
+				perms.add(PosixFilePermission.OWNER_WRITE);
+				break;
+			case WRITE:
+				perms.add(PosixFilePermission.OWNER_WRITE);
+				break;
+			case WRITE_EXECUTE:
+				perms.add(PosixFilePermission.OWNER_WRITE);
+				perms.add(PosixFilePermission.OWNER_EXECUTE);
+				break;
 
-            default:
-                break;
-            }
-            switch (permission.getGroupAction()) {
-            case ALL:
-                perms.add(PosixFilePermission.GROUP_READ);
-                perms.add(PosixFilePermission.GROUP_WRITE);
-                perms.add(PosixFilePermission.GROUP_EXECUTE);
-                break;
-            case EXECUTE:
-                perms.add(PosixFilePermission.GROUP_EXECUTE);
-                break;
-            case NONE:
+			default:
+				break;
+			}
+			switch (permission.getGroupAction()) {
+			case ALL:
+				perms.add(PosixFilePermission.GROUP_READ);
+				perms.add(PosixFilePermission.GROUP_WRITE);
+				perms.add(PosixFilePermission.GROUP_EXECUTE);
+				break;
+			case EXECUTE:
+				perms.add(PosixFilePermission.GROUP_EXECUTE);
+				break;
+			case NONE:
 
-                break;
-            case READ:
-                perms.add(PosixFilePermission.GROUP_READ);
-                break;
-            case READ_EXECUTE:
-                perms.add(PosixFilePermission.GROUP_READ);
-                perms.add(PosixFilePermission.GROUP_EXECUTE);
-                break;
-            case READ_WRITE:
-                perms.add(PosixFilePermission.GROUP_READ);
-                perms.add(PosixFilePermission.GROUP_WRITE);
-                break;
-            case WRITE:
-                perms.add(PosixFilePermission.GROUP_WRITE);
-                break;
-            case WRITE_EXECUTE:
-                perms.add(PosixFilePermission.GROUP_WRITE);
-                perms.add(PosixFilePermission.GROUP_EXECUTE);
-                break;
+				break;
+			case READ:
+				perms.add(PosixFilePermission.GROUP_READ);
+				break;
+			case READ_EXECUTE:
+				perms.add(PosixFilePermission.GROUP_READ);
+				perms.add(PosixFilePermission.GROUP_EXECUTE);
+				break;
+			case READ_WRITE:
+				perms.add(PosixFilePermission.GROUP_READ);
+				perms.add(PosixFilePermission.GROUP_WRITE);
+				break;
+			case WRITE:
+				perms.add(PosixFilePermission.GROUP_WRITE);
+				break;
+			case WRITE_EXECUTE:
+				perms.add(PosixFilePermission.GROUP_WRITE);
+				perms.add(PosixFilePermission.GROUP_EXECUTE);
+				break;
 
-            default:
-                break;
-            }
-            switch (permission.getOtherAction()) {
-            case ALL:
-                perms.add(PosixFilePermission.OTHERS_READ);
-                perms.add(PosixFilePermission.OTHERS_WRITE);
-                perms.add(PosixFilePermission.OTHERS_EXECUTE);
-                break;
-            case EXECUTE:
-                perms.add(PosixFilePermission.OTHERS_EXECUTE);
-                break;
-            case NONE:
+			default:
+				break;
+			}
+			switch (permission.getOtherAction()) {
+			case ALL:
+				perms.add(PosixFilePermission.OTHERS_READ);
+				perms.add(PosixFilePermission.OTHERS_WRITE);
+				perms.add(PosixFilePermission.OTHERS_EXECUTE);
+				break;
+			case EXECUTE:
+				perms.add(PosixFilePermission.OTHERS_EXECUTE);
+				break;
+			case NONE:
 
-                break;
-            case READ:
-                perms.add(PosixFilePermission.OTHERS_READ);
-                break;
-            case READ_EXECUTE:
-                perms.add(PosixFilePermission.OTHERS_READ);
-                perms.add(PosixFilePermission.OTHERS_EXECUTE);
-                break;
-            case READ_WRITE:
-                perms.add(PosixFilePermission.OTHERS_READ);
-                perms.add(PosixFilePermission.OTHERS_WRITE);
-                break;
-            case WRITE:
-                perms.add(PosixFilePermission.OTHERS_WRITE);
-                break;
-            case WRITE_EXECUTE:
-                perms.add(PosixFilePermission.OTHERS_WRITE);
-                perms.add(PosixFilePermission.OTHERS_EXECUTE);
-                break;
+				break;
+			case READ:
+				perms.add(PosixFilePermission.OTHERS_READ);
+				break;
+			case READ_EXECUTE:
+				perms.add(PosixFilePermission.OTHERS_READ);
+				perms.add(PosixFilePermission.OTHERS_EXECUTE);
+				break;
+			case READ_WRITE:
+				perms.add(PosixFilePermission.OTHERS_READ);
+				perms.add(PosixFilePermission.OTHERS_WRITE);
+				break;
+			case WRITE:
+				perms.add(PosixFilePermission.OTHERS_WRITE);
+				break;
+			case WRITE_EXECUTE:
+				perms.add(PosixFilePermission.OTHERS_WRITE);
+				perms.add(PosixFilePermission.OTHERS_EXECUTE);
+				break;
 
-            default:
-                break;
-            }
-        }
-        URI uri = p.toUri();
-        java.nio.file.Path nioPath = null;
-        try {
-            uri = uri.getFragment() == null ? uri : new URI(uri.getScheme(), uri.getSchemeSpecificPart(), null);
-            if (uri.getScheme() == null) {
-                String path = uri.getPath();
-                if (!path.startsWith("/")) {
-                    nioPath = Paths.get(workingDir.toUri().getPath(), path);
-                } else {
-                    uri = new URI("file:///" + path);
-                    nioPath = Paths.get(uri);
-                }
+			default:
+				break;
+			}
+		}
+		URI uri = p.toUri();
+		java.nio.file.Path nioPath = null;
+		try {
+			uri = uri.getFragment() == null ? uri : new URI(uri.getScheme(), uri.getSchemeSpecificPart(), null);
+			if (uri.getScheme() == null) {
+				String path = uri.getPath();
+				if (!path.startsWith("/")) {
+					nioPath = Paths.get(workingDir.toUri().getRawPath(), path);
+				} else {
+					uri = new URI("file:///" + path);
+					nioPath = Paths.get(uri.getRawPath());
+				}
 
-            } else {
-                nioPath = Paths.get(uri);
-            }
-        } catch (URISyntaxException e) {
-            throw new IOException(e);
-        }
-        Files.setPosixFilePermissions(nioPath, perms);
-        return perms;
-    }
+			} else {
+				nioPath = Paths.get(uri.getRawPath());
+			}
+		} catch (URISyntaxException e) {
+			throw new IOException(e);
+		}
+		Files.setPosixFilePermissions(nioPath, perms);
+		return perms;
+	}
 }
